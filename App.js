a2b0aee47acac06e812e348596b4587ad2e61bfa
irfan-emifusion <irfan.emifusion@gmail.com>
2021-01-07 16:33:50 +0500

    Initial Stage App

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.container} />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 100,
+    height: 200,
+    backgroundColor: 'dodgerblue',
+  },
+});
+
+export default App;
